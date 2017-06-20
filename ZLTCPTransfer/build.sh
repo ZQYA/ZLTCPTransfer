a0e099b4c15002d7e2ea70c7577d151f219d57b3
林盼 <linpan@qiyi.com>
2017-06-20 17:46:04 +0800

    modify script

--- ZLTCPTransfer/build.sh
+++ ZLTCPTransfer/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-FRM_name="TCPTransfer"
+FRM_name="ZLTCPTransfer"
+FRM_path="./build/Release/"
 
 MMTP_path="../../MMTP"
 MMTP_branch="develop"
@@ -9,20 +10,22 @@ TCPTransfer_path=`pwd`
 TCPTransfer_branch="develop"
 
 #update MMTP
-cd ${MMTP_path}
-git checkout .
-git clean -df
-git fetch
-git checkout ${MMTP_branch}
-git rebase origin/${MMTP_branch}
+#cd ${MMTP_path}
+#git checkout .
+#git clean -df
+#git fetch
+#git checkout ${MMTP_branch}
+#git rebase origin/${MMTP_branch}
 
 #update ZLTCPTransfer
-cd ${TCPTransfer_path}
-git checkout .
-git clean -df
-git fetch
-git checkout ${TCPTransfer_branch}
-git rebase origin/${TCPTransfer_branch}
+#cd ${TCPTransfer_path}
+#git checkout .
+#git clean -df
+#git fetch
+#git checkout ${TCPTransfer_branch}
+#git rebase origin/${TCPTransfer_branch}
 
 #build
-#xcodebuild -configuration "Release" -target "${FRM_name}"  clean build
+xcodebuild -configuration "Release" -target "${FRM_name}"  clean build
+
+open ${FRM_path}

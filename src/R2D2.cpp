79d925068250af977da131727d1129f095cba632
周权 <zhouquan02@bilibili.com>
2017-06-20 17:15:50 +0800

    add const

--- src/R2D2.cpp
+++ src/R2D2.cpp
@@ -5,7 +5,7 @@
 #include <cstring>
 #include <cstdio>
 #include "luke.hpp"
-int testclient(char *host, int port) {
+int testclient(const char *host, int port) {
 	SOCKET sk_fd = dk_socket();
 	sockaddr_in server_addr;
 	server_addr.sin_addr.s_addr = inet_addr(host);
@@ -13,7 +13,6 @@ int testclient(char *host, int port) {
 	dk_connect(sk_fd,(const struct sockaddr *)&server_addr,sizeof(struct sockaddr_in));
 	char teststr[] = "hello world";
 	int write_size = mp_write(sk_fd,teststr,strlen(teststr),0,true);
-	printf("write size:%d",write_size);	
 	return write_size;
 }
 

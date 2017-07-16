5d37445f9ded9f3e138786afe82ed2a5405d4aff
zhou quan <zhouquan02@bilibili.com>
2017-07-16 14:10:44 +0800

    fix cpp bug

--- src/dklog.cpp
+++ src/dklog.cpp
@@ -1,5 +1,8 @@
 #include "dklog.hpp"
 #include <cstdio>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fstream>
 /********************************************************************************
  * define a class to process the log
  * every level has a file outputstream , higher level will cover the lower level's 
@@ -12,61 +15,61 @@
 class dk_output_stream {
 private:
 	int level; 
-	std::ostream error_stream;   
-	std::ostream warning_stream;
-	std::ostream info_stream;
+	std::ostream *error_stream;   
+	std::ostream *warning_stream;
+	std::ostream *info_stream;
 public:
-	dk_output_stream(int _level):level(_level) const {
+	dk_output_stream(int _level):level(_level) {
 		std::cout << "level:" << level << std::endl;
-		const char *home_path  = getenv("HOME");
-		const char *log_dir = "/dk_log";
-		char *log_path_in_home = (char *)malloc(1024);
-		
+	    this->error_stream = this->dk_create_ostream(L_ERROR);
+        this->warning_stream = this->dk_create_ostream(L_WARNING);
+        this->info_stream = this->dk_create_ostream(L_INFO);
 	} 
 	
 	
-	dk_output_stream & operator<T><<(const  T t) {
+	template <typename T> dk_output_stream & operator<<(const  T t) {
 		if(this->level >= L_ERROR) {
-				
-		}	
+			*error_stream<<t;
+        }
+        if(this->level >= L_WARNING){
+            *warning_stream<<t;
+		}
+        if(this->level >= L_INFO){
+            *info_stream<<t;
+		}
+        if(this->level >= L_DEBUG ) {
+            std::cout<<t;    
+        }
 	}  
 
 
-	std::ostream dk_create_ostream(int level) const {
-		char *name = level>=L_ERROR?"/dk_error.log":(level>=L_WARNING?"/dk_warning":(level>=L_INFO?"/dk_info":NULL));	
+	std::ostream* dk_create_ostream(int level) const {
+		const char *name = level>=L_ERROR?"/dk_error.log":(level>=L_WARNING?"/dk_warning":(level>=L_INFO?"/dk_info":NULL));	
 		if(name == NULL) {
 			return NULL;	
 		}else {
 			char *file_path = (char *)malloc(1024);
 			bzero(file_path,1024);
 			char *home_path = getenv("HOME");	
-			char *level_path = "/dk_logd";
+			const char *level_path = "/dk_logd";
 			strcat(file_path,home_path);
 			strcat(file_path,level_path);
 			if(0!=access(file_path,F_OK)) {
-				int res = mkdir(filepath,0777);			
+				int res = mkdir(file_path,0777);			
 				if(res!=0) {
 					std::cerr<<"create log path failed"<<file_path<<std::endl;
 				}
 			}
 			strcat(file_path,name);
-			std::ofstream fstm(file_path);
+			std::ofstream *fstm = new std::ofstream(std::string(file_path),std::ios::out);
 			free(file_path);
 			return fstm;
 		}
 	}
-}
+};
 
-
-void log(int level,char *fmt, ...) {	
-	switch (level) { 
-			case L_DEBUG:
-				break;
-			case L_INFO:
-				break;
-			case L_WARNING:
-				break;
-			case L_ERROR:
-				break;
-	}
+int main(int args , const char **argv) {
+    dk_output_stream s(L_ERROR);
+    s<<"test error"<<3<<"nuil"; 
+    return 0;
 }
